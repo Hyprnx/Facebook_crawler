a47a3b9d26519fd3bb519df7014373ba1f3cb324
Hyprnx <toducanh2001@gmail.com>
2022-04-12 20:17:47 +0700

    update

--- resources/clone_acc.py
+++ resources/clone_acc.py
@@ -1,10 +1,3 @@
-# list_tk_clone = ['frost.wyrm318@gmail.com', '0855112000',
-#                  "hoangtrung20223108@gmail.com", 'anhtuan3182022@gmail.com', 'anhtuan3192022@gmail.com']
-# list_mk_clone = ['12345678hihi', 'luongvuthien15102000',
-#                  "Thang202111!", "Thang202111!", "Thang202111!"]
-
-# https://m.facebook.com/camtu.nguyen.73744/about
-
 clone_acc = {
     "frost.wyrm318@gmail.com": "abcxyz",
     "0855112000": "luongvuthien15102000",
